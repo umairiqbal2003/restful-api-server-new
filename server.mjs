24b0ad0f7671bb32578552e60582605b10e712de
Umair Iqbal <umairazeem1999@gmail.com>
2022-08-21 14:18:28 +0500

    update server

--- server.mjs
+++ server.mjs
@@ -9,27 +9,109 @@ const port = process.env.PORT ||  3000;
 
 let users = [];
 
+/// generate random number for user
+
+function randomNumber(){
+
+  return Math.floor(Math.random() * 10000000)
+}
+
 app.post('/user', (req, res) => {
     console.log(req.body);
 
-    users.push(req.body);
+    let newUser = {
+     
+      id:randomNumber(),
+        fullname: req.body.userfullname,
+        Email      : req.body.userEmail,
+        Password   : req.body.userPassword,
+        City : req.body.userCity
+
+    }
+
+    users.push(newUser);
 
     res.send("user is created" );
 })
-app.get('/user', (req,res)=>{
-    res.send(users)
+app.get('/user/:userId', (req,res)=>{
+    
+    let userId = req.params.userId
+    
+    let isFound = false;
+
+    for( i=0 ; i < users.length ; i++){
+
+      if(users[i].id == userId){
+        res.send(users[i])
+        isFound = true;
+        break;
+      }
+    }
+   if(!isFound){
+    res.send("user is not found")
+   } 
+
+})
+app.get('/users', (req,res)=>{
+  res.send(users)
 })
 
-app.get('/', (req, res) => {
-  console.log("aik request server pe i")
-    res.send('Hello World!')
+app.put('/user/:userId', (req, res) => {
+  res.send('Your user is modified')
+  
+  let userId = req.params.userId;
+  let userIndex = -1;
+  
+  for( i=0 ; i < users.length ; i++){
+
+    if(users[i].id == userId){
+      userIndex = i
+      break;
+    }
+  }
+  if(userIndex === -1){
+    res.send("user is not found")
+  }else{
+   if(req.body.userfullname) users[userIndex].fullname = req.body.userfullname
+    if(req.body.userEmail)   users[userIndex].Email = req.body.userEmail
+   if(req.body.userPassword) users[userIndex].Password = req.body.userPassword
+   if(req.body.userCity)     users[userIndex].City = req.body.userCity
+
+  res.send(users[userIndex])
+  }
+  
+
 })
 
-app.get('/profile', (req, res) => {
-    console.log("aik request server pe i")
-      res.send('Hello World!')
+app.delete('/user/:userId', (req, res) => {
+
+        
+  let userId = req.params.userId;
+  let userIndex = -1;
+  
+  for( i=0 ; i < users.length ; i++){
+
+    if(users[i].id == userId){
+      userIndex = i
+      break;
+    }
+  }
+  if(userIndex === -1){
+    res.send("user is not found")
+  }else{
+    users.splice(userIndex,1);
+    res.send('Your user is modified')
+    
+  }
+
   })
 
+  app.delete('/users', (req, res) => {
+    // console.log("aik request server pe i")
+
+    users = [];
+      res.send('Hello World!')
+  })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
